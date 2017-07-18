d8d2717c698c5af4050c78583f89d30baa6fdb84
Oscar Gerardo Lazo Arjona <oscar.lazoarjona@physics.ox.ac.uk>
2017-07-18 17:26:12 +0100

    Stiff method that works.

--- examples/orca/simple_example.py
+++ examples/orca/simple_example.py
@@ -13,18 +13,22 @@ from quantum_memories import orca
 from quantum_memories.misc import set_parameters_ladder, efficiencies
 import numpy as np
 
+# [6.34, 8.77, 14.50, 19.37]
+# [6.50, 9.85, 14.58, 19.92]
 
-default_params = set_parameters_ladder({"Temperature": 273.15+90,
-                                        "alpha_rw": np.sqrt(20.0)})
+cus_params = {"Temperature": 273.15+90,
+              "alpha_rw": np.sqrt(20.0), "Nz": 50}
+params = set_parameters_ladder(cus_params)
+
 if __name__ == '__main__':
     name = "test"
 
     # Benchmark with plotting.
     t0 = time()
-    t, Z, vZ, rho, Om1 = orca.solve(default_params, plots=True, name=name)
+    t, Z, vZ, rho, Om1 = orca.solve(params, plots=True, name=name)
     tsolve = time()-t0
     t0 = time()
-    eff_in, eff_out, eff = efficiencies(t, Om1, default_params,
+    eff_in, eff_out, eff = efficiencies(t, Om1, params,
                                         plots=True, name=name)
     teff = time()-t0
     nfun = 0
@@ -35,15 +39,14 @@ if __name__ == '__main__':
 
     # Benchmark without plotting.
     t0 = time()
-    t, Z, vZ, rho, Om1 = orca.solve(default_params, plots=False, name=name)
+    t, Z, vZ, rho, Om1 = orca.solve(params, plots=False, name=name)
     tsolve = time()-t0
     t0 = time()
-    eff_in, eff_out, eff = efficiencies(t, Om1, default_params,
+    eff_in, eff_out, eff = efficiencies(t, Om1, params,
                                         plots=False, name=name)
     teff = time()-t0
-
-    print
-    print "Not including plotting times:"
+    nfun = 0
+    print "Including plotting times:"
     print "The solve function took", tsolve, "s."
     print "The efficiencies function took", teff, "s."
     print "The efficiencies were:", eff_in, eff_out, eff
