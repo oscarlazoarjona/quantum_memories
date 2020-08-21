3d4e2c843328bf10340a66245a6c4d0ffd7e7a48
oscarlazoarjona <oscar.lazoarjona@physics.ox.ac.uk>
2020-08-21 15:37:59 +0100

    Removed examples.

---
+++ examples/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Compatible with Python 2.7.xx
+# Copyright (C) 2020 Oscar Gerardo Lazo Arjona
+# mailto: oscar.lazoarjona@physics.ox.ac.uk
+r"""Python module initializer."""
