c7578cbe9cd52ce6432f439df79f0d59c64bad72
oscarlazoarjona <oscar.lazoarjona@physics.ox.ac.uk>
2020-08-21 15:34:26 +0100

    Removed build dist and egg files.

--- build/lib/quantum_memories/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2017 Oscar Gerardo Lazo Arjona
-# mailto: oscar.lazoarjona@physics.ox.ac.uk
-r"""This is a repository for solvers for the Maxwell-Bloch equations of various
-quantum memories, and calculations using them.
-"""
-
-__version__ = "0.1"
